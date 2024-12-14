519f5798a655fb2970d2b169e2fcc32c5fa712dc
AgavFens <144495302+AgavFens@users.noreply.github.com>
2024-12-14 08:52:01 +0300

    Add files via upload

---
+++ WebApplication11/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication11.Models;
+
+public partial class Cart
+{
+    public int UserId { get; set; } // Ссылка на пользователя
+    public int ProductId { get; set; } // Ссылка на продукт
+    public int Quantity { get; set; } // Количество продукта
+
+    public virtual User User { get; set; }
+    public virtual Catalog Product { get; set; }
+}
